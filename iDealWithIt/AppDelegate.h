bdee9abd6b0ab8c4f660de2d0ca8300dd6a3e1ce
Mark Olson <theothermarkolson@gmail.com>
2012-06-16 16:52:01 -0400

    Initial Commit

---
+++ iDealWithIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iDealWithIt
+//
+//  Created by mark olson on 6/16/12.
+//  Copyright (c) 2012 syntaxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ iDealWithIt/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  iDealWithIt
+//
+//  Created by mark olson on 6/16/12.
+//  Copyright (c) 2012 syntaxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end

2b19bc6392843533b8a89b50f27b41c4f54756ee
mark <theothermarkolson@gmail.com>
2012-07-03 22:05:18 -0400

    I have no idea what I'm doing.

---
+++ iDealWithIt/UploadViewController.h
@@ -0,0 +1,13 @@
+//
+//  UploadViewController.h
+//  iDealWithIt
+//
+//  Created by Mark on 7/3/12.
+//  Copyright (c) 2012 syntaxi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UploadViewController : UIViewController
+
+@end

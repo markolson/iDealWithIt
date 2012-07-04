2b7cd75e231f2f50d13a83da3c907f7066378e97
mark <theothermarkolson@gmail.com>
2012-07-04 01:39:57 -0400

    suspiciously easy...

--- iDealWithIt/UploadViewController.h
+++ iDealWithIt/UploadViewController.h
@@ -8,10 +8,16 @@
 
 #import <UIKit/UIKit.h>
 #import "FaceViewController.h"
+#import <ImageIO/ImageIO.h>
+#import <MobileCoreServices/MobileCoreServices.h>
 
 #import "FBUser.h"
+#import "MBProgressHUD.h"
+#import "ImageOverlay.h"
 
 @interface UploadViewController : UIViewController
 
 @property (nonatomic, retain) FaceViewController *parent;
+@property (nonatomic, retain) IBOutlet UIImageView *overlay;
+@property (nonatomic, retain) MBProgressHUD *hud;
 @end
--- iDealWithIt/UploadViewController.m
+++ iDealWithIt/UploadViewController.m
@@ -8,24 +8,26 @@
 
 #import "UploadViewController.h"
 
+@interface UploadViewController (Background)
+
+- (void)exportThread:(NSString *)fileOutput;
+@end
+
 @interface UploadViewController ()
 
 @end
 
 @implementation UploadViewController
 
-@synthesize parent;
+@synthesize parent, overlay, hud;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
         // Custom initialization
-        NSLog(@"oh no!");
-        FBUser *f = [[FBUser alloc] init];
+        //FBUser *f = [[FBUser alloc] init];
     }
-    NSLog(@"oh no!");
-    FBUser *f = [[FBUser alloc] init];
     return self;
 }
 
@@ -33,6 +35,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    [parent.optionBar setItems:@[] animated:NO];
+    [self render];
 }
 
 - (void)didReceiveMemoryWarning
@@ -45,5 +49,69 @@
 {
     return (interfaceOrientation == UIInterfaceOrientationPortrait);
 }
+
+-(void)render
+{
+    NSString * tempFile = [NSString stringWithFormat:@"%@/%ld", NSTemporaryDirectory(), time(NULL)];
+    NSLog(@"Saving to.. %@", tempFile);
+    hud = [MBProgressHUD showHUDAddedTo:self.view animated:NO];
+    //[self.view addSubview:hud];
+    
+	hud.removeFromSuperViewOnHide = YES;
+	hud.mode = MBProgressHUDModeIndeterminate;
+    hud.labelText = @"Drawing";
+    
+    
+	[NSThread detachNewThreadSelector:@selector(exportThread:) toTarget:self withObject:tempFile];
+    
+}
+
+
+#pragma mark - Background Thread -
+
+- (void)exportThread:(NSString *)path {
+	@autoreleasepool {
+        path = [path stringByAppendingPathExtension:@"gif"];
+        ImageOverlay *io = [[ImageOverlay alloc] initWithFaces:parent.faces andDimensions:overlay.bounds.size];
+        [io setFrames:11];
+        
+        CGImageDestinationRef destination = CGImageDestinationCreateWithURL((CFURLRef)[NSURL fileURLWithPath:path],
+                                                                            kUTTypeGIF,
+                                                                            [io frame_count],
+                                                                            NULL);
+        
+        NSLog(@"%@", (NSString *)kCGImagePropertyGIFDictionary);
+        NSDictionary *gFrames = @{ (NSString *)kCGImagePropertyGIFDictionary: @{@"DelayTime" : @0.2 }};
+        NSDictionary *lFrame = @{ (NSString *)kCGImagePropertyGIFDictionary: @{@"DelayTime" : @2 }};
+        
+        NSDictionary *gifProperties = [NSDictionary dictionaryWithObject:[NSDictionary dictionaryWithObject:[NSNumber numberWithInt:0] forKey:(NSString *)kCGImagePropertyGIFLoopCount] forKey:(NSString *)kCGImagePropertyGIFDictionary];
+        
+
+		CGSize canvasSize = parent.image.size;
+        for (int i = 0; i < [io frame_count]; i++) {
+            UIGraphicsBeginImageContext( canvasSize );
+            [parent.image drawAtPoint:CGPointMake(0, 0)];
+            [[io nextFrame] drawInRect:CGRectMake(0,0, canvasSize.width,canvasSize.height) blendMode:kCGBlendModeNormal alpha:1.0];
+            UIImage * image = UIGraphicsGetImageFromCurrentImageContext();
+            
+            if([io isLastFrame]) {
+                NSLog(@"ALMOST");
+                CGImageDestinationAddImage(destination, image.CGImage, (CFDictionaryRef)lFrame);
+            }else{
+                CGImageDestinationAddImage(destination, image.CGImage, (CFDictionaryRef)gFrames);
+            }
+            
+            UIGraphicsEndImageContext();
+            
+        }
+        
+        CGImageDestinationSetProperties(destination, (CFDictionaryRef)gifProperties);
+        CGImageDestinationFinalize(destination);
+        CFRelease(destination);
+        NSLog(@"animated GIF file created at %@", path);
+        [hud hide:YES];
+	}
+}
+
 
 @end
